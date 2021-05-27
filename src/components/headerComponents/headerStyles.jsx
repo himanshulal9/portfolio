27251b80ff47e86f05e7acda164b15dd5087dac0
himanshulal9 <himanshulal9@gmail.com>
2021-05-27 19:17:50 +0530

    fixed bug in navbar

--- src/components/headerComponents/headerStyles.jsx
+++ src/components/headerComponents/headerStyles.jsx
@@ -21,7 +21,7 @@ export const useStyles = makeStyles((theme) => ({
     alignItems: "center",
   },
   navbar: {
-    backgroundColor: Theme.colors.primary,
+    backgroundColor: Theme.colors.primary1,
     color: Theme.colors.base2,
   },
   displayContainer: {
